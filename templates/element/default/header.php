091924a9bfe56f322860727b0c77dc581da4532d
Austin-T-Grant <agrantdeveloper@gmail.com>
2020-08-12 22:06:47 +0200

    Updated Favicon

--- templates/element/default/header.php
+++ templates/element/default/header.php
@@ -5,7 +5,6 @@
     <?= $this->Html->meta('viewport', 'width=device-width, initial-scale=1'); ?>
     <title><?= $title ?></title>
     <?= $this->Html->meta('icon'); ?>
-    <?= $this->Html->css(['normalize.min', 'milligram.min', 'cake']); ?>
     <?= $this->Html->css('default/style.css'); ?>
     <?= $this->fetch('meta'); ?>
     <?= $this->fetch('css'); ?>
